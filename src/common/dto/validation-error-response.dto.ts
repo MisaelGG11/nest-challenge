cd4e20826ab943b9566ebdab027bfa71fc3b72ad
MisaelGG11 <gg20031@ues.edu.sv>
2025-11-02 00:44:03 -0600

    fix: api error response dto

--- src/common/dto/validation-error-response.dto.ts
+++ src/common/dto/validation-error-response.dto.ts
@@ -11,4 +11,19 @@ export class ValidationErrorResponse {
     oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
   })
   message: string | string[];
+
+  @ApiProperty()
+  path: string;
+
+  @ApiProperty()
+  method: string;
+
+  @ApiProperty()
+  requestId: string;
+
+  @ApiProperty()
+  timestamp: string;
+
+  @ApiProperty()
+  details: unknown;
 }
